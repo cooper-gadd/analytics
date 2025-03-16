483ccee2023c5db1072a27f8ee374f16d3304103
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-03-16 10:57:10 -0400

    Add Frosted UI

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Theme } from "frosted-ui";
+import "frosted-ui/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Theme appearance="dark">{children}</Theme>
       </body>
     </html>
   );
